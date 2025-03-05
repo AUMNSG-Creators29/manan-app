467164325c404f97685c441e56008402ebe57595
AUMNSG-Creators29 <nirnaysgodlaw@gmail.com>
2025-03-05 14:06:13 +0530

    Update firebase.js with configuration

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
+  apiKey: "AIzaSyBcGe9-fvjFDlz8BO9wQfk7KsxoTqMOu2Q",
   authDomain: "manan-9fea8.firebaseapp.com",
   projectId: "manan-9fea8",
-  storageBucket: "manan-9fea8.appspot.com",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
+  storageBucket: "manan-9fea8.firebasestorage.app",
+  messagingSenderId: "538027010059",
+  appId: "1:538027010059:web:649d93256ca87d57403f0e"
 };
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
